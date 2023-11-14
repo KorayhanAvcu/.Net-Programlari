46b1b21feb4e7242a932b639f36cdef559bf9e19
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2023-11-15 02:22:33 +0300

    Add files via upload

---
+++ C#_Tip_Donusumleri/Program.cs
@@ -0,0 +1,54 @@
+﻿// Implicit Conversion (Bilinçsiz Dönüşüm)
+Console.WriteLine("******* Implicit Conversion **********");
+byte a = 5;
+sbyte b = 30;
+short c = 10;
+int d = a+b+c;
+Console.WriteLine("d:" + d);
+long h = d;
+Console.WriteLine("h:" + h);
+float i= h;
+Console.WriteLine("i:" + i);
+string e="zikriye";
+char f='k';
+object g = e+f+d;
+Console.WriteLine("g:" + g);
+//Explicit Conversion (Bilinçli Dönüşüm)
+Console.WriteLine("***** Explicit Conversion *****");
+int x = 4;
+byte y = (byte)x;
+Console.WriteLine("y:" +y);
+int z = 100;
+byte t = (byte)z;
+Console.WriteLine("t:" +t);
+
+float w = 10.3f;
+byte v = (byte)w;
+Console.WriteLine("v:" +v);
+// ***** ToString Methodu ******
+Console.WriteLine("******* ToString Methodu *******");
+int xx = 6;
+string yy = xx.ToString();
+Console.WriteLine("yy:" +yy);
+string zz = 12.5f.ToString();
+Console.WriteLine("zz:" + zz);
+//System.Convert
+Console.WriteLine("***** System.Convert sınıfı *******");
+string s1 = "10", s2="20";
+int sayil, sayi2;
+int Toplam;
+sayil = Convert.ToInt32 (s1);
+sayi2 = Convert.ToInt32 (s2);
+Toplam = sayil + sayi2;
+Console.WriteLine("Toplam:" + Toplam);
+
+//Parse
+Console.WriteLine("********* Parse Methodu******");
+string metin1 = "10";
+string metin2 = "10.25";
+int rakam1;
+double double1;
+rakam1= Int32.Parse(metin1);
+double1= Double.Parse(metin2);
+Console.WriteLine("rakaml1 :" + rakam1);
+Console.WriteLine("doublel :" + double1);

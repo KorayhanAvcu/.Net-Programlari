c7a837fdb3a7b72ac112cd120a706bd41a9cb457
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2023-11-19 20:09:03 +0300

    Add files via upload

---
+++ rekursif_extension/Program.cs
@@ -0,0 +1,65 @@
+﻿//rekursif 
+Islemler instance = new();
+Console.WriteLine(instance.Expo(3,4));
+
+
+//extension
+
+string ifade = "Korayhan Avcu";
+bool sonuc = ifade.CheckSpaces();
+
+Console.WriteLine(sonuc);
+if(sonuc){
+    Console.WriteLine(ifade.RemoveWhiteSpaces());
+}
+Console.WriteLine(ifade.MakeUpperCase());
+Console.WriteLine(ifade.MakeLowerCase());
+
+int[] dizi = {9,3,6,2,1,5,0};
+dizi.SortArray();
+dizi.EkranaYazdir();
+int sayi=5;
+Console.WriteLine(sayi.IsEvenNumber());
+Console.WriteLine(ifade.GetFirstCharacter());
+public class Islemler{
+   public int Expo(int sayi, int us) {
+        if (us == 1) 
+            return sayi;
+        else
+            return Expo(sayi, us - 1) * sayi; 
+    }
+}
+
+public static class Extension{
+    public static bool CheckSpaces(this string param){
+        return param.Contains(" "); //içerisinde boslu var ise bunun bana döndür
+    }
+    public static string RemoveWhiteSpaces(this string param){
+        string[] dizi=param.Split(" ");
+        return string.Join("",dizi);
+    }
+    public static string MakeUpperCase(this string param){
+        
+        return param.ToUpper();
+    }
+    public static string MakeLowerCase(this string param){
+        
+        return param.ToLower();
+    }
+    public static int[] SortArray(this int[] param){
+        Array.Sort(param);
+        return param;
+    }
+    public static void EkranaYazdir(this int[] param){
+        foreach(var item in param){
+            Console.WriteLine(item);
+        }
+    }
+    public static bool IsEvenNumber(this int param){
+        return param%2==0;
+    }
+
+    public static string GetFirstCharacter(this string param){
+        return param.Substring(0,1);
+    }
+}

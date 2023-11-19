b29b1877ee84222fa0d94cc63f2194937d03301f
KorayhanAvcu <84989228+KorayhanAvcu@users.noreply.github.com>
2023-11-19 14:06:19 +0300

    Add files via upload

---
+++ methodlar/Program.cs
@@ -0,0 +1,40 @@
+﻿
+int a = 2;
+int b = 3;
+
+Console.WriteLine(a+b);
+int sonuc = Topla(a,b);
+Console.WriteLine(sonuc);
+
+
+Metotlar ornek = new Metotlar();
+ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+int sonuc2=ornek.ArttirVeTopla(a,b);
+int sonuc3=ornek.ArttirVeTopla(ref a,ref b);
+ornek.EkranaYazdir(Convert.ToString(sonuc2));
+ornek.EkranaYazdir(Convert.ToString(a+b));
+int Topla(int deger1,int deger2){
+	return (deger1+deger2);
+}
+
+
+
+class Metotlar{
+	public void EkranaYazdir(string veri){
+		Console.WriteLine(veri);
+	}
+	public int ArttirVeTopla(int deger1, int deger2){
+		deger1+=1;
+		deger2+=1;
+		return deger1+deger2;
+	}
+
+public int ArttirVeTopla(ref int deger1,ref int deger2){
+		deger1+=1;
+		deger2+=1;
+		return deger1+deger2;
+	}
+
+
+}
